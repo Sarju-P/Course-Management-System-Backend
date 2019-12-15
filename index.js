f56165e6fa174c39c0fdc7f5e7d562dd98e8adcb
Sarju-P <charming.sap@gmail.com>
2019-12-14 19:52:12 -0500

    Getting the list of Courses

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+// Load Express Module : Returns a function
+const express = require ('express');
+
+// When calling the function, it returns object of type express
+const app=express();
+
+const courses= [
+    {
+        id:1, name: 'course 1'
+    },
+    {
+        id:2, name: 'course 2'
+    },
+    {
+        id:3, name: 'course 3'
+    }
+]
+// app object has bunch of useful methods like get/post/put and delete
+app.get('/', (req,res)=> {
+    res.send(courses);
+});
+
+app.listen(3000,()=>{
+    console.log('Listening on port 3000');
+});
