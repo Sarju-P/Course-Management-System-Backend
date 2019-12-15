103262e953829974beb21ac57c67ab26774e7685
Sarju-P <charming.sap@gmail.com>
2019-12-14 20:54:42 -0500

    Added environment variable PORT and also its explanation

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ app.get('/', (req,res)=> {
 app.get('/courses',(req,res)=>{
     res.send(courses);
 })
-app.listen(3000,()=>{
-    console.log('Listening on port 3000');
+
+/*When ee have hardcode the value of port, thouugh it may work in development environment, but we cannot be sure that it works in 
+production environment, because the port is dynamically assigned in hosting environment.So, we can't rely on the port to be avaibale. 
+So, the way to fix this is by using environment variable.
+
+Typically in hosting environment or node applications, we have the environment variable called PORT.
+
+Env Varaible is a variable that is part of the environment in which a process runs.Its value is set outside the application.
+We have the global object called process, which has a property called env, then we give the name of our environment variable, 
+which is PORT for us now.
+
+If it is set, we will use it otherwise, we will use 3000.
+*/
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}...`);
 });
+
+/* In my machine, if I didn't have environment variable called port, it would use 3000.
+Command to set environment variable is : 
+export (Mac) /set (Windows) PORT=5000 (To use port with a value of 5000) */
+
